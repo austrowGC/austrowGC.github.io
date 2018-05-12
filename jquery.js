719264ac29a7b9ee4764564fa2eda9898e1dd57f
Gregory Austrow <gregoryaustrow@gmail.com>
2018-05-11 21:50:33 -0400

    change layout
    new layout, menu, header

--- jquery.js
+++ jquery.js
@@ -2,4 +2,6 @@
 $(document).ready(function () {
     $("#main").addClass("clearfix");
     $("#main div").addClass("container foreground");
+    $("#header").addClass("menu");
+    $("#header div:not(#title)").addClass("menu submenu");
 });
